bcee1087943bff29bbbbdd5e2987f5f545b2a4db
Joe <joe@nayya.com>
2025-07-14 23:59:23 -0400

    feat(docs): add some docs

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Setup Bruno
+Setup Bruno to generate bruno collection but they are already committed.
 `npm install -g @usebruno/cli`
 
-Setup Poetry
+[Install Bruno](https://www.usebruno.com/) and open the collection from bruno collection if you want to mess with the endpoints. You'll need to [create an environmet](https://docs.usebruno.com/variables/environment-variables) and add `baseUrl` with a value of `http://localhost:8000`
+
+[Setup Poetry if you want](https://python-poetry.org/docs/) but running `docker compose up --build` will install and work with poetry in the container.
+
+Docker will spin up the app, and a postgres DB that it initializes with seed data from `data/init.sql`
--- app/crud.py
+++ app/crud.py
@@ -4,6 +4,21 @@ from datetime import date
 import uuid
 
 
+def ensure_nested_ids(data):
+    """Ensure all nested objects have UUIDs"""
+    if isinstance(data, dict):
+        # Generate ID if not present
+        if 'id' not in data or not data['id']:
+            data['id'] = str(uuid.uuid4())
+        # Recursively process nested objects
+        for key, value in data.items():
+            if isinstance(value, (dict, list)):
+                data[key] = ensure_nested_ids(value)
+    elif isinstance(data, list):
+        return [ensure_nested_ids(item) for item in data]
+    return data
+
+
 def get_patients(db: Session, cursor: str = None, limit: int = 20):
     query = db.query(models.Patient)
     
@@ -29,18 +44,24 @@ def get_patient(db: Session, patient_id: str):
 
 
 def create_patient(db: Session, patient: schemas.PatientCreate):
+    # Process medical info to ensure medications have UUIDs
+    medical_info = patient.medicalInfo.model_dump()
+    medical_info['current_medications'] = ensure_nested_ids(medical_info.get('current_medications', []))
+    
+    # Process documents to ensure they have UUIDs
+    documents = ensure_nested_ids([doc.model_dump() for doc in patient.documents])
+    
     db_patient = models.Patient(
-        id=str(uuid.uuid4()),
         first_name=patient.firstName,
         last_name=patient.lastName,
         date_of_birth=patient.dateOfBirth,
         email=patient.email,
         phone=patient.phone,
-        address=patient.address.dict(),
-        emergency_contact=patient.emergencyContact.dict(),
-        medical_info=patient.medicalInfo.dict(),
-        insurance=patient.insurance.dict(),
-        documents=[doc.model_dump() for doc in patient.documents],
+        address=patient.address.model_dump(),
+        emergency_contact=patient.emergencyContact.model_dump(),
+        medical_info=medical_info,
+        insurance=patient.insurance.model_dump(),
+        documents=documents,
     )
     db.add(db_patient)
     db.commit()
@@ -52,8 +73,18 @@ def update_patient(db: Session, patient_id: str, updates: dict):
     db_patient = get_patient(db, patient_id)
     if not db_patient:
         return None
+    
     for key, value in updates.items():
+        if key == 'medical_info' and isinstance(value, dict):
+            # Ensure medications have UUIDs
+            if 'current_medications' in value:
+                value['current_medications'] = ensure_nested_ids(value['current_medications'])
+        elif key == 'documents' and isinstance(value, list):
+            # Ensure documents have UUIDs
+            value = ensure_nested_ids(value)
+        
         setattr(db_patient, key, value)
+    
     db_patient.updated_at = date.today()
     db.commit()
     db.refresh(db_patient)
